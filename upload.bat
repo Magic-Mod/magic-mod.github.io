55441e04801161a957dbfa0c451bf968c6554a3d
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-06-26 11:52:53 -0400

    Upload main source code.

--- upload.bat
+++ upload.bat
@@ -2,6 +2,6 @@
 git init
 git add .
 git commit -m "Upload main source code."
-git branch -M main
-git remote add origin https://github.com/Magic-Mod/magicmod.github.io.git
-git push -f --no-verify origin main
+git branch -M develop
+git remote add origin https://github.com/Magic-Mod/magic-mod.github.io.git
+git push -f --no-verify origin develop
