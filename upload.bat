a4512de666fc84b13f884ccc40abdb23123c3200
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-06-26 13:55:37 -0400

    Add files via upload

--- upload.bat
+++ upload.bat
@@ -5,3 +5,6 @@ git commit -m "Upload main source code."
 git branch -M develop
 git remote add origin https://github.com/Magic-Mod/magic-mod.github.io.git
 git push -f --no-verify origin develop
+
+git push https://I4Z2eYspW55qufl8sRjFqtf2ghbbCqVtF4ots5pEPjmLLDDj@github.com/Magic-Mod/magic-mod.github.io.git develop
+PAUSE
